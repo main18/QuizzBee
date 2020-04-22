456a682b812f18551b9c32c92c443a992dd65328
unknown <ayman.bmh123@gmail.com>
2020-04-22 22:47:23 +0000

    Updates

---
+++ precache-manifest.71d550cffd8088494dba8eb1c94d6e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c379fed9b47adce26b78e5122003b36",
+    "url": "/QuizzBee/index.html"
+  },
+  {
+    "revision": "fffbb1e11cecdd2bb0c3",
+    "url": "/QuizzBee/static/css/main.6110cfc1.chunk.css"
+  },
+  {
+    "revision": "be85144c6ef3ae37d77f",
+    "url": "/QuizzBee/static/js/2.c937230b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/QuizzBee/static/js/2.c937230b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fffbb1e11cecdd2bb0c3",
+    "url": "/QuizzBee/static/js/main.38978d5d.chunk.js"
+  },
+  {
+    "revision": "e3ad5e96f28bb31b5617",
+    "url": "/QuizzBee/static/js/runtime-main.57b3ea78.js"
+  }
+]);
